b43ec66d61af15fcd78fe7f7c0b5f0806829f53c
piyushprasad8122-creator <piyushprasad8122@gmail.com>
2026-04-10 06:50:05 +0000

    Fix: update tunnel URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://certificate-gauge-precision-translation.trycloudflare.com/api'
+  baseURL: 'https://download-curriculum-life-accomplish.trycloudflare.com/api'
 });
 
 API.interceptors.request.use((config) => {
