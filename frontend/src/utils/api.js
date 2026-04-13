d2a640a713035746189a353ee850dd0ce2d45b92
piyushprasad8122-creator <piyushprasad8122@gmail.com>
2026-04-13 06:20:59 +0000

    Fix: update tunnel URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://invalid-mechanisms-bite-fiction.trycloudflare.com/api'
+  baseURL:  'https://tough-gates-typically-chronicle.trycloudflare.com/api'
 });
 
 API.interceptors.request.use((config) => {
